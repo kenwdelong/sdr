57bff96c9e6a340e630a867e210f9e044d535bd2
ken <ken@pop-os.localdomain>
2024-04-15 19:31:39 -0700

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+Run the Geekworm file
+
+sudo apt update
+sudo apt upgrade
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py  && pip3 install --break-system-packages ansible==7.5.0
