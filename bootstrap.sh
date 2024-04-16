35510e31c13bcefc71329ddf99b2a3da4a9a856a
Ken DeLong <kenwdelong@gmail.com>
2024-04-15 21:57:30 -0700

    missed an edit

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@ Run the Geekworm file (GeekwormNSAPI.txt)
 
 sudo apt update
 sudo apt upgrade
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py  && pip3 install --break-system-packages ansible==7.5.0
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py  && pip3 install --break-system-packages ansible
