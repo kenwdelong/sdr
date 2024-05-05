00ee8d13d976f96542bb74f5ccae77c1b1446ccb
ken <ken@pop-os.localdomain>
2024-05-05 12:53:43 -0700

    updates for new style and to use venv

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,13 @@ Run the Geekworm file (GeekwormNSAPI.txt)
 
 sudo apt update
 sudo apt upgrade
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py  && pip3 install --break-system-packages ansible==7.5.0
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py  && pip3 install --break-system-packages ansible==7.5.0
+
+python3 -m venv ./ansible-venv
+source ./ansible-venv/bin/activate
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py
+pip install ansible
+
+git clone https://github.com/kenwdelong/sdr.git
+
+ansible-playbook sdr.yml
